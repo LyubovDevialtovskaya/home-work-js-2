8bfc315722be125be1d13d5a5e4f9e4b87142cc4
LyubovDevialtovskaya <luba1297mail@gmail.com>
2021-06-17 23:15:33 +0300

    второе дз

---
+++ js/main.js
@@ -0,0 +1,166 @@
+console.log('Sample JavaScript #2 HW #16');
+
+
+var userObj = {
+  age: 24,
+  firstName: 'Lyubov',
+  lastName: 'Devialtouskaya',
+  fullName() {
+    return `${userObj.firstName} ${userObj.lastName}`;
+  }
+};
+console.log(userObj.fullName());
+
+function defUpperStr(str) {
+  return (str || 'Default text').toUpperCase();
+}
+console.log(defUpperStr('My text'));
+console.log(defUpperStr());
+function evenFn(n) {
+}
+function evenFn(n) {
+  let arr = [];
+
+  for (let i = 1; i <= n; i++) if (i % 2 === 0) arr.push(i);
+
+  return arr;
+}
+
+console.log(evenFn(10));
+console.log(evenFn(15));
+console.log(evenFn(20));
+
+
+function weekFn(cond) {
+  let str = '';
+
+  switch (cond) {
+    case 1:
+      str = 'Понедельник';
+      break;
+    case 2:
+      str = 'Вторник';
+      break;
+    case 3:
+      str = 'Среда';
+      break;
+    case 4:
+      str = 'Четверг';
+      break;
+    case 5:
+      str = 'Пятница';
+      break;
+    case 6:
+      str = 'Суббота';
+      break;
+    case 7:
+      str = 'Воскресенье';
+      break;
+    default:
+      str = null;
+  }
+
+  return str;
+}
+
+console.log(weekFn(1));
+console.log(weekFn(3));
+console.log(weekFn(7));
+console.log(weekFn(9));
+console.log(weekFn(1.5));
+console.log(weekFn('2'));
+
+
+function ageClassification(num) {
+  return num > 0
+    ? num > 24
+      ? num > 44
+        ? num > 65
+          ? num > 75
+            ? num > 90
+              ? num > 122
+                ? null
+                : 'долгожители'
+              : 'старческий возраст'
+            : 'пожилой возраст'
+          : 'средний возраст'
+        : 'молодой возраст'
+      : 'детский возраст'
+    : null;
+}
+
+
+console.log('    -1 :', ageClassification(-1)); // -1 : null
+console.log('     1 :', ageClassification(1)); // 1 : детский возраст
+console.log('    24 :', ageClassification(24)); // 24 : детский возраст
+console.log(' 24.01 :', ageClassification(24.01)); // 24.01 : молодой возраст
+console.log('    44 :', ageClassification(44)); // 44 : молодой возраст
+console.log(' 44.01 :', ageClassification(44.01)); // 44.01 : средний возраст
+console.log('    65 :', ageClassification(65)); // 65 : средний возраст
+console.log('  65.1 :', ageClassification(65.1)); // 65.1 : пожилой возраст
+console.log('    75 :', ageClassification(75)); // 75 : пожилой возраст
+console.log(' 75.01 :', ageClassification(75.01)); // 75.01 : старческий возраст
+console.log('    90 :', ageClassification(90)); // 90 : старческий возраст
+console.log(' 90.01 :', ageClassification(90.01)); // 90.01 : долгожители
+console.log('   122 :', ageClassification(122)); // 122 : долгожители
+console.log('122.01 :', ageClassification(122.01)); // 122.01 : null
+console.log('   150 :', ageClassification(150)); // 150 : null
+
+
+console.log('    -1 :', ageClassification(-1) === null); // -1 : null
+console.log('     1 :', ageClassification(1) === 'детский возраст'); // 1 : детский возраст
+console.log('    24 :', ageClassification(24) === 'детский возраст'); // 24 : детский возраст
+console.log(' 24.01 :', ageClassification(24.01) === 'молодой возраст'); // 24.01 : молодой возраст
+console.log('    44 :', ageClassification(44) === 'молодой возраст'); // 44 : молодой возраст
+console.log(' 44.01 :', ageClassification(44.01) === 'средний возраст'); // 44.01 : средний возраст
+console.log('    65 :', ageClassification(65) === 'средний возраст'); // 65 : средний возраст
+console.log('  65.1 :', ageClassification(65.1) === 'пожилой возраст'); // 65.1 : пожилой возраст
+console.log('    75 :', ageClassification(75) === 'пожилой возраст'); // 75 : пожилой возраст
+console.log(' 75.01 :', ageClassification(75.01) === 'старческий возраст'); // 75.01 : старческий возраст
+console.log('    90 :', ageClassification(90) === 'старческий возраст'); // 90 : старческий возраст
+console.log(' 90.01 :', ageClassification(90.01) === 'долгожители'); // 90.01 : долгожители
+console.log('   122 :', ageClassification(122) === 'долгожители'); // 122 : долгожители
+console.log('122.01 :', ageClassification(122.01) === null); // 122.01 : null
+console.log('   150 :', ageClassification(150) === null); // 150 : null
+
+
+
+
+function oddFn(n) {
+  let arr = [];
+  let i = 0;
+
+  while (i++ < n) if (i % 2 !== 0) arr.push(i);
+
+  return arr;
+}
+
+console.log(oddFn(10)); // [1, 3, 5, 7, 9]
+console.log(oddFn(15)); // [1, 3, 5, 7, 9, 11, 13, 15]
+console.log(oddFn(20)); // [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+
+
+function mainFunc(a, b, cb) {
+  if (cb && typeof cb === 'function') return cb(a, b);
+
+  return false;
+}
+
+
+function cbRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function cbPow(num, pow) {
+  return Math.pow(num, pow);
+}
+
+function cbAdd(a, b) {
+  return a + b;
+}
+
+
+console.log(mainFunc(2, 5, cbRandom)); // целые числа в диапазоне 2..5
+console.log(mainFunc(2, 5, cbPow)); // 32
+console.log(mainFunc(2, 5, cbAdd)); // 7
+console.log(mainFunc(2, 5, 'not a func')); // false
